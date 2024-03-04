6a9c885f7befb0183789b2cf8d4b50d39738632b
Alican Dökmen <alican@outlook.com>
2024-03-04 12:30:02 +0300

    homeviewmodel

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Web.Interfaces;
 using Web.Models;
+using Web.Services;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHomeViewModelService _homeViewModelService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHomeViewModelService homeViewModelService)
         {
             _logger = logger;
+            _homeViewModelService = homeViewModelService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? categoryId, int? brandId)
         {
-            return View();
+            var vm = await _homeViewModelService.GetHomeViewModelAsync(categoryId, brandId);
+            return View(vm);
         }
 
         public IActionResult Privacy()
